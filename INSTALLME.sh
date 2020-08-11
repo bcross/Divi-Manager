c24a001dc6d508fe8a0106fd53fbedf6b4889135
Bryan Cross <bryan@thecrossfamily.org>
2020-08-10 20:59:29 -0500

    Create INSTALLME.sh

---
+++ INSTALLME.sh
@@ -0,0 +1,11 @@
+if [ $EUID -ne 0 ]; then
+  echo "Run this script with elevated privileges."
+  exit 2
+fi
+wget -q https://github.com/bcross/Divi-Manager/archive/master.zip
+unzip -qqo master.zip
+rm master.zip Divi-Manager-master/INSTALLME Divi-Manager-master/LICENSE Divi-Manager-master/README.md
+sudo cp -rf Divi-Manager-master/* /usr/local/bin
+sudo chmod +x /usr/local/bin/divi-mgr
+rm Divi-Manager-master -r
+rm ${BASH_SOURCE[0]}

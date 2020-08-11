bf96ce8fa3ab6249c2b67015d7db350b79b2194d
Bryan Cross <bryan@thecrossfamily.org>
2020-08-11 08:41:18 -0500

    Update verify.sh

--- divi-mgr-lib/verify.sh
+++ divi-mgr-lib/verify.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 function divi-mgr-verify {
-    echo "Coming soon..."
-    return 1
     echo "Waiting for divid to be ready..."
     ready=0
     while [ $ready -eq 0 ]; do
@@ -17,10 +15,22 @@ function divi-mgr-verify {
     done
     allgood=0
     blockcount=$(divi-cli getblockcount)
+    #If stuck at block 100 for 3 minutes, repair
+    while [ $blockcount -eq 100 ] && [ $badtime -le 18 ]; do
+        sleep 10
+        blockcount=$(divi-cli getblockcount)
+        let "badtime++"
+    done
+    #Get the block hash locally
     blockhash=$(divi-cli getblockhash $blockcount)
-    trustedblockhash
+    #Get the block hash from a trusted source
+    trustedblockhash=$(wget -qO- https://chainz.cryptoid.info/divi/api.dws?q=getblockhash&height=$blockcount)
+    #If the block count is greater than 100 and the block hashes match, all is well
+    if [ $blockcount -gt 100 ] && [ "$blockhash" == "$trustedblockhash" ]; then
+        allgood=1
+    fi
     if [ $allgood -eq 0 ]; then
-    divi-mgr repair
+        divi-mgr repair
     fi
 }
 
